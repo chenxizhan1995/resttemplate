3292cf37a85efca33969cdff05eb90c3be2952e5
chenxizhan <chenxizhan1995@163.com>
2022-05-31 22:59:11 +0800

    RestTemplate发送文件-待续

--- src/test/java/com/one/learn/resttemplate/RestTemplateDemo3.java
+++ src/test/java/com/one/learn/resttemplate/RestTemplateDemo3.java
@@ -14,6 +14,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -28,17 +29,18 @@ public class RestTemplateDemo3 {
      * 它会对经过此代理的HTTP请求抓包，方便学习
      */
     @Before
-    public void setProxy(){
+    public void setProxy() {
         System.out.println("set proxy");
         System.setProperty("http.proxyHost", "localhost");
         System.setProperty("http.proxyPort", "8888");
     }
+
     /**
      * 名值对发送
      * 1. 使用 MultiValueMap<String, String>  做请求体，自动按名值对处理
      */
     @Test
-    public void postNv(){
+    public void postNv() {
         RestTemplate template = new RestTemplate();
 
         MultiValueMap<String, String> mvs = new LinkedMultiValueMap<>();
@@ -57,16 +59,16 @@ public class RestTemplateDemo3 {
      * multipart 请求
      * 1. 一直没有找到令人满意的解答。
      * 2. 勉强可以接受的解答，
-     *      手动设置 multipart/form-data 首部
-     *      用 MultiValueMap<String, FileSystemResource> 做请求体
-     *
-     *  此示例，表明，multipart 下的
-     *      - FileSystemResource 会自动补充文件上传相关的首部
-     *      - 字符串，会按纯文本处理
+     * 手动设置 multipart/form-data 首部
+     * 用 MultiValueMap<String, FileSystemResource> 做请求体
+     * <p>
+     * 此示例，表明，multipart 下的
+     * - FileSystemResource 会自动补充文件上传相关的首部
+     * - 字符串，会按纯文本处理
      */
 
     @Test
-    public void postFile(){
+    public void postFile() {
         RestTemplate template = new RestTemplate();
 
         FileSystemResource resource = new FileSystemResource("src/test/resources/data");
@@ -81,16 +83,17 @@ public class RestTemplateDemo3 {
         HttpEntity<MultiValueMap<String, ?>> entity = new HttpEntity<>(body, headers);
         String res = template.postForObject("http://httpbin.org/post", entity, String.class);
     }
+
     /**
      * 接上，FileSystemResource 方式上传文件毕竟不够通用
-     *
+     * <p>
      * ClassPathResource 呢？
      * 仍然能按文件处理 Content-Disposition: form-data; name="file2"; filename="data"
-     *
+     * <p>
      * 它与FileSystemResource 都有 filename 方法
      */
     @Test
-    public void postFile2(){
+    public void postFile2() {
         RestTemplate template = new RestTemplate();
 
         Resource resource = new ClassPathResource("/data");
@@ -104,10 +107,11 @@ public class RestTemplateDemo3 {
         HttpEntity<MultiValueMap<String, ?>> entity = new HttpEntity<>(body, headers);
         String res = template.postForObject("http://httpbin.org/post", entity, String.class);
     }
+
     /**
-     *  承上，再通用一些，InputStream 呢？
-     *  哈，报错了 org.springframework.http.converter.HttpMessageNotWritableException: Could not write request:
-     *      no suitable HttpMessageConverter found for request type [java.io.BufferedInputStream]
+     * 承上，再通用一些，InputStream 呢？
+     * 哈，报错了 org.springframework.http.converter.HttpMessageNotWritableException: Could not write request:
+     * no suitable HttpMessageConverter found for request type [java.io.BufferedInputStream]
      */
     @Test
     public void postFile3() throws IOException {
@@ -146,6 +150,7 @@ public class RestTemplateDemo3 {
         HttpEntity<MultiValueMap<String, ?>> entity = new HttpEntity<>(body, headers);
         String res = template.postForObject("http://httpbin.org/post", entity, String.class);
     }
+
     /**
      * 有个 ContentDisposition 类，是干嘛的？
      * 貌似找到了
@@ -182,6 +187,7 @@ public class RestTemplateDemo3 {
     /**
      * Q. 是否 HttpEntity 的嵌套自然生成 multipart 首部？
      * 是的
+     *
      * @throws IOException
      */
 
@@ -199,12 +205,14 @@ public class RestTemplateDemo3 {
 
         String res = template.postForObject("http://httpbin.org/post", mainBody, String.class);
     }
+
     /**
      * 有个 ContentDisposition 类，会自动进行 url 编码吗？
      * Ans：不会
      * 那我要它何用？
      * 唯一的用途大概是不用手动写入首部的名称了。
      * 可是，如果我理解这个首部的用法，记下对应的首部名称，也不难啊！！！
+     *
      * @throws IOException
      */
     @Test
@@ -229,5 +237,96 @@ public class RestTemplateDemo3 {
 
         String res = template.postForObject("http://httpbin.org/post", mainBody, String.class);
     }
+    //---------------------------------
+    // 发送文件的各种姿势
+
+    /**
+     * 直接发送 File 对象
+     * Ans: 不行，实际发送的是文件路径
+     */
+    @Test
+    public void postFile8() {
+        RestTemplate template = new RestTemplate();
+        File file = new File("src/test/resources/data");
+        String res = template.postForObject("http://httpbin.org/post", file, String.class);
+    }
+
+    /**
+     * 发送 FileSystemResource
+     * Ans:不行，application/octet-stream
+     */
+    @Test
+    public void postFile9() {
+        RestTemplate template = new RestTemplate();
+        File file = new File("src/test/resources/data");
+        String res = template.postForObject("http://httpbin.org/post", new FileSystemResource(file), String.class);
+    }
+
+    /**
+     * 指定 multipart/form-data 首部，+ File 对象
+     * Ans：不行，No HttpMessageConverter for java.io.File and content type "multipart/form-data"
+     */
+    @Test
+    public void postFile10() {
+        RestTemplate template = new RestTemplate();
+        File file = new File("src/test/resources/data");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity entity = new HttpEntity(file, headers);
+        String res = template.postForObject("http://httpbin.org/post", entity, String.class);
+    }
+
+    /**
+     * 指定 multipart/form-data 首部，+ FileSystemResource 对象
+     * Ans: 请求报文不对，只是首部有变化，请求体没有自动调整，八成是预期开发者自动处理请求体？
+     */
+    @Test
+    public void postFile11() {
+        RestTemplate template = new RestTemplate();
+        File file = new File("src/test/resources/data");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity entity = new HttpEntity(new FileSystemResource(file), headers);
+        String res = template.postForObject("http://httpbin.org/post", entity, String.class);
+    }
+
+    /**
+     * 指定 multipart/form-data 首部，+ Mv 封装的 FileSystemResource 对象
+     * Ans: 可以
+     */
+    @Test
+    public void postFile12() {
+        RestTemplate template = new RestTemplate();
+        File file = new File("src/test/resources/data");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Resource> multiValueMap = new LinkedMultiValueMap<>();
+        multiValueMap.add("file", new FileSystemResource(file)  );
+
+        HttpEntity entity = new HttpEntity(multiValueMap, headers);
+        String res = template.postForObject("http://httpbin.org/post", entity, String.class);
+    }
+
+    /**
+     * 指定 multipart/form-data 首部，+ Mv 封装的 File 对象
+     * Ans: 不行。虽然拆分了，但是按照 application/json 发送了文件路径
+     */
+    @Test
+    public void postFile13() {
+        RestTemplate template = new RestTemplate();
+        File file = new File("src/test/resources/data");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> multiValueMap = new LinkedMultiValueMap<>();
+        multiValueMap.add("file", file );
+
+        HttpEntity entity = new HttpEntity(multiValueMap, headers);
+        String res = template.postForObject("http://httpbin.org/post", entity, String.class);
+    }
 }
+
 
